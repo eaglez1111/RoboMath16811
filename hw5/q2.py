730f6569310edb47cbc3deddaa07176922dcbf3c
Eagle Zhao <tim.eagle.zhao@gmail.com>
2019-11-19 18:28:22 -0500

    working on Dijkstra now

--- hw5/q2.py
+++ hw5/q2.py
@@ -13,15 +13,30 @@ npa = np.array
 def inPolygon(p,CH):
     N = len(CH)
     for ch in CH:
+        if p in ch: continue
         n = len(ch)
+        inFlag = 1
         for j in range(n):
             k = (j+1)%n
-            if not q1.isLeft(ch[j],ch[k],p):
-                return False
-    return True
+            inFlag *= q1.isLeft(ch[j],ch[k],p)
+        if inFlag:
+            return True
+    return False
 
+def commonPolygon(p0,p1,CH):
+    N = len(CH)
+    for ch in CH:
+        if p0 in ch and p1 in ch:
+            a, b, n = np.argwhere(ch.T[0]==p0[0]), np.argwhere(ch.T[0]==p1[0]), len(ch)
+            if (a==0 and b==n-1) or (b==0 and a==n-1) or (abs(a-b)==1):
+                return 2
+            else:
+                return 1
+    return 0
 
 def intersected(p1,p2,p3,p4):  # line p1-p2 and line p3-p4
+    if p1[0]==p3[0] or p1[0]==p4[0] or p2[0]==p3[0] or p2[0]==p4[0]:
+        return False
     x_1, y_1 = p1
     x_2, y_2 = p2
     x_3, y_3 = p3
@@ -58,17 +73,34 @@ V = [pStart]
 for ch in Obstacles:
     for p in ch:
         if not inPolygon(p,Obstacles):
-            V.append(p)
+            V.append(tuple(p))
 V.append(pEnd)
+
 
 nV = len(V)
 costMat = np.inf*np.ones([nV,nV])
 for i in range(nV):
     for j in range(nV):
-        if isFreePath(V[i],V[j],Obstacles):
-            d_ = dist(npa(V[i])-npa(V[j]))
-            costMat[i,j] = d_
-            costMat[j,i] = d_
+        if i==j:
+            costMat[i,j] = 0
+            continue
+        cpFlag = commonPolygon(V[i],V[j],Obstacles) # 1 means same polygon not neighboring, 2 means same polygon and neighboring, 0 means different polygon
+        if cpFlag==1:
+            continue
+        if cpFlag==0 and (not isFreePath(V[i],V[j],Obstacles)):
+            continue
+        d_ = dist(npa(V[i])-npa(V[j]))
+        costMat[i,j] = d_
+        costMat[j,i] = d_
+
+
+
+''' Dijkstra '''
+cost = np.inf*np.ones(nV)
+S, U = [V[0]], V[1:]
+cost[0] = 0
+while(len(U)>0):
+    updateCost(p,)
 
 
 
@@ -82,6 +114,9 @@ for ch in Obstacles:
 for i in range(len(V)):
     plt.plot(V[i][0], V[i][1],'*r',label='')
     plt.text(V[i][0], V[i][1],str(i),label='')
+    for j in range(i,len(V)):
+        if costMat[i,j]<np.inf:
+            plt.plot([V[i][0],V[j][0]], [V[i][1],V[j][1]],'-g',label='')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('')
